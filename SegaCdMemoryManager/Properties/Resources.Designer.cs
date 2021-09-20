46f5f995e71398e8b4185b1f7c9860ae586a8dfd
J.C. Fields <jcfields@jcfields.dev>
2021-09-20 08:17:56 -0400

    moved icons

--- SegaCdMemoryManager/Properties/Resources.Designer.cs
+++ SegaCdMemoryManager/Properties/Resources.Designer.cs
@@ -259,25 +259,5 @@ namespace SegaCdMemoryManager.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap image_model1 {
-            get {
-                object obj = ResourceManager.GetObject("image_model1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap image_model2 {
-            get {
-                object obj = ResourceManager.GetObject("image_model2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
